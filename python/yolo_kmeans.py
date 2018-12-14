e7af8efff7c50a2cdad0695b1e601ae03ba6cad0
Andy <yutzuliuhf@gmail.com>
2018-12-15 02:13:37 +0800

    k means

--- python/yolo_kmeans.py
+++ python/yolo_kmeans.py
@@ -3,9 +3,9 @@ import numpy as np
 
 class YOLO_Kmeans:
 
-    def __init__(self, cluster_number, filename):
+    def __init__(self, cluster_number):
         self.cluster_number = cluster_number
-        self.filename = "2012_train.txt"
+       
 
     def iou(self, boxes, clusters):  # 1 box -> k clusters
         n = boxes.shape[0]
@@ -57,8 +57,8 @@ class YOLO_Kmeans:
 
         return clusters
 
-    def result2txt(self, data):
-        f = open("yolo_anchors.txt", 'w')
+    def result2txt(self, data,output):
+        f = open(output, 'w')
         row = np.shape(data)[0]
         for i in range(row):
             if i == 0:
@@ -68,8 +68,8 @@ class YOLO_Kmeans:
             f.write(x_y)
         f.close()
 
-    def txt2boxes(self):
-        f = open(self.filename, 'r')
+    def txt2boxes(self,filename):
+        f = open(filename, 'r')
         dataSet = []
         for line in f:
             infos = line.split(" ")
@@ -84,18 +84,66 @@ class YOLO_Kmeans:
         f.close()
         return result
 
-    def txt2clusters(self):
-        all_boxes = self.txt2boxes()
+    def tfrecord2boxes(self,filename):
+        import tensorflow as tf
+        
+        def parse_exmp(serial_exmp):
+            feats = tf.parse_single_example(serial_exmp, features={
+                #'image/filename':tf.FixedLenFeature([], tf.string),
+                #'image/encoded':tf.FixedLenFeature([1], tf.string),
+                'image/height':tf.FixedLenFeature([1], tf.int64),
+                'image/width':tf.FixedLenFeature([1], tf.int64),
+                #'image/object/class/label':tf.VarLenFeature( tf.int64),
+                #'image/object/class/index':tf.VarLenFeature( tf.int64),
+                'image/object/bbox/xmin':tf.VarLenFeature( tf.float32),
+                'image/object/bbox/xmax':tf.VarLenFeature( tf.float32),
+                'image/object/bbox/ymin':tf.VarLenFeature( tf.float32),
+                'image/object/bbox/ymax':tf.VarLenFeature( tf.float32)
+            })
+            return feats
+        dataset = tf.data.TFRecordDataset(RECORDS)
+        dataset = dataset.map(parse_exmp).batch(1)
+        iter_dataset   = dataset.make_one_shot_iterator().get_next()
+        with tf.Session() as sess:  
+            result=np.empty(shape=[0, 2])
+            while True:
+                try:
+                    parsed_features=sess.run(iter_dataset)
+                    w=np.around(parsed_features["image/object/bbox/xmax"].values*parsed_features['image/width'])
+                    -np.around(parsed_features["image/object/bbox/xmin"].values*parsed_features['image/width'])
+                    h=np.around(parsed_features["image/object/bbox/ymax"].values*parsed_features['image/height'])
+                    -np.around(parsed_features["image/object/bbox/ymin"].values*parsed_features['image/height'])
+                    boxs=np.vstack((w,h)).astype(np.int32).T
+                    result=np.vstack((result,boxs))
+                except tf.errors.OutOfRangeError:
+                    return result     
+
+    def data2clusters(self,output,all_boxes):
+       
         result = self.kmeans(all_boxes, k=self.cluster_number)
         result = result[np.lexsort(result.T[0, None])]
-        self.result2txt(result)
+        self.result2txt(result,output)
         print("K anchors:\n {}".format(result))
         print("Accuracy: {:.2f}%".format(
             self.avg_iou(all_boxes, result) * 100))
 
 
 if __name__ == "__main__":
-    cluster_number = 9
-    filename = "2012_train.txt"
-    kmeans = YOLO_Kmeans(cluster_number, filename)
-    kmeans.txt2clusters()
+    # data from
+    data_from=1
+    # 0: tf-records
+    RECORDS = ['dataset/raccoon/a.record']
+    # 1: orginal txt file
+    ANNOTATIONS = "model_data/raccoon_annotations.txt"
+
+    # args
+    LEN_anchors = 9
+    OUTPUT_anchors='model_data/raccoon_anchors2.txt'
+
+
+    kmeans = YOLO_Kmeans(LEN_anchors)
+    if data_from==1:
+        all_boxes = kmeans.txt2boxes(ANNOTATIONS)
+    else:
+        all_boxes = kmeans.tfrecord2boxes(RECORDS)
+    kmeans.data2clusters(OUTPUT_anchors,all_boxes)
